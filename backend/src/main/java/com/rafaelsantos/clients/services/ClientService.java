0ce1f9a30c6dc0eba05d7c72ec190c7e436efc45
Rafael Santos <rafa.05125@gmail.com>
2022-11-26 14:20:31 -0300

    UPDATE client

--- backend/src/main/java/com/rafaelsantos/clients/services/ClientService.java
+++ backend/src/main/java/com/rafaelsantos/clients/services/ClientService.java
@@ -2,6 +2,8 @@ package com.rafaelsantos.clients.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -39,6 +41,18 @@ public class ClientService {
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 	
 	
