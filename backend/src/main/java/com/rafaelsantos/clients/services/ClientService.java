9b989d52e299ad58e47da9260f95fa09811d7359
Rafael Santos <rafa.05125@gmail.com>
2022-11-26 14:29:22 -0300

    DELETE client

--- backend/src/main/java/com/rafaelsantos/clients/services/ClientService.java
+++ backend/src/main/java/com/rafaelsantos/clients/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.rafaelsantos.clients.dto.ClientDTO;
 import com.rafaelsantos.clients.entities.Client;
 import com.rafaelsantos.clients.repositories.ClientRepository;
+import com.rafaelsantos.clients.services.exceptions.DatabaseException;
 import com.rafaelsantos.clients.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,18 @@ public class ClientService {
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 	
