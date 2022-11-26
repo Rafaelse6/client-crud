a59a4c0014556bce6e3a446cce73b34d90efb93a
Rafael Santos <rafa.05125@gmail.com>
2022-11-26 13:22:54 -0300

    Find by id and exceptions

--- backend/src/main/java/com/rafaelsantos/clients/services/ClientService.java
+++ backend/src/main/java/com/rafaelsantos/clients/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.rafaelsantos.clients.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rafaelsantos.clients.dto.ClientDTO;
 import com.rafaelsantos.clients.entities.Client;
 import com.rafaelsantos.clients.repositories.ClientRepository;
+import com.rafaelsantos.clients.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,5 +23,12 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged(PageRequest pageable){
 		Page<Client> list = repository.findAll(pageable);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
